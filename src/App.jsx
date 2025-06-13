4c19b12b3f68d4af6595db2598e65ee3be34217a
Rem Simiyu <remsimiyu@gmail.com>
2025-06-14 02:34:39 +0300

    Project Management

--- src/App.jsx
+++ src/App.jsx
@@ -13,22 +13,38 @@ const handleStartAddProject = () => {
   setProjectsState(prevState => {
     return{
       ...prevState,
-      selectedProjectId: null,
+      selectedProjectId: undefined,
     };
+  });
+}
+
+const handleAddProject = (projectData) => {
+  setProjectsState((prevState) => {
+    const projectId = Math.random();
+    const newProject = {
+      ...projectData,
+      id: projectId
+    };
+
+    return {
+      ...prevState,
+      selectedProjectId: undefined,
+      projects: [...prevState.projects, newProject]
+    }
   });
 }
 
 let content;
 
 if(projectsState.selectedProjectId === null) {
-  content = <NewProject />
+  content = <NewProject onAdd={handleAddProject}/>
 } else if (projectsState.selectedProjectId === undefined) {
   content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
 }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
       {content}
       </main>
   );
